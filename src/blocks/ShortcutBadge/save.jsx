d44486b925681c662e0039e5af45e6c6e2a0e39b
Heyde Moura <moura.heyde@gmail.com>
2023-10-03 13:10:29 -0300

    Fix style hierarchy on ShortcutBadge block

--- src/blocks/ShortcutBadge/save.jsx
+++ src/blocks/ShortcutBadge/save.jsx
@@ -3,10 +3,12 @@ import "./saveStyle.scss";
 
 export default function save(props) {
   return (
-    <a href={props.attributes.value} target="_blank" rel="noreferrer">
-      <div className="daxor__block__shortcut-badge">
-        <p>{props.attributes.label}</p>
-      </div>
-    </a>
+    <div className="daxor__block__shortcut-badge">
+      <a href={props.attributes.value} target="_blank" rel="noreferrer">
+        <div className="daxor__block__shortcut-badge-content">
+          <p>{props.attributes.label}</p>
+        </div>
+      </a>
+    </div>
   );
 }
