387b1bc29ae7da047e31e39855d3ecc918752a9b
Heyde Moura <moura.heyde@gmail.com>
2023-10-08 18:58:39 -0300

    Add block options to editor toolbar

--- src/components/EditorToolBar.jsx
+++ src/components/EditorToolBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Flex, Button } from "@radix-ui/themes";
 import { LuPanelRight } from "react-icons/lu";
+import { BlockToolbar } from "@wordpress/block-editor";
 import "./EditorToolBar.scss";
 
 const EditorToolBar = ({ onInspectorToggleClick }) => {
   return (
-    <Flex className="daxor__editor-toolbar" justify="end">
-      <Button variant="outline" onClick={onInspectorToggleClick}>
+    <Flex
+      className="daxor__editor-toolbar"
+      justify="space-between"
+      align="center"
+    >
+      <Flex>
+        <BlockToolbar />
+      </Flex>
+      <Button variant="outline" onClick={onInspectorToggleClick} size="4">
         <LuPanelRight />
       </Button>
     </Flex>
