1dd85660bc591122b25f903b042aeb0a2d1abc28
Heyde Moura <moura.heyde@gmail.com>
2023-10-08 23:11:53 -0300

    Fix save method for radix-columns

--- src/blocks/radix-columns/save.js
+++ src/blocks/radix-columns/save.js
@@ -1,8 +1,12 @@
 import { Grid, Flex } from "@radix-ui/themes";
+import { useInnerBlocksProps, useBlockProps } from "@wordpress/block-editor";
 
 const Save = (props) => {
   const { columns } = props.attributes;
-  return <Grid columns={columns || 1}></Grid>;
+  const blockProps = useBlockProps.save();
+  const innerBlockProps = useInnerBlocksProps.save(blockProps);
+
+  return <Flex gap="3" {...innerBlockProps}></Flex>;
 };
 
 export default Save;
