44edc324df446405c9cd9728b09550c1c90aaa35
Heyde Moura <moura.heyde@gmail.com>
2023-10-08 22:50:10 -0300

    remove useBlockProps from radix-columns save

--- src/blocks/radix-columns/save.js
+++ src/blocks/radix-columns/save.js
@@ -1,8 +1,6 @@
 import { Grid, Flex } from "@radix-ui/themes";
-import { useBlockProps } from "@wordpress/blocks";
 
 const Save = (props) => {
-  const blockProps = useBlockProps.save();
   const { columns } = props.attributes;
   return <Grid columns={columns || 1}></Grid>;
 };
