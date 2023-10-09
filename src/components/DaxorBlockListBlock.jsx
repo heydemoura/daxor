6ba5808090ca0b5cca29bc0149bba073b20e6dfe
Heyde Moura <moura.heyde@gmail.com>
2023-10-08 21:09:32 -0300

    Exclude stacked card styling from layout blocks

--- src/components/DaxorBlockListBlock.jsx
+++ src/components/DaxorBlockListBlock.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { createHigherOrderComponent } from "@wordpress/compose";
 import { Card, Inset } from "@radix-ui/themes";
 
+const excludeBlocks = ["core/column", "core/columns", "core/paragraph"];
+
 const DaxorBlockListBlock = createHigherOrderComponent(
   (BlockListBlock) => (props) => {
-    if (["core/column", "core/columns"].indexOf(props.name) > -1) {
+    if (excludeBlocks.indexOf(props.name) > -1) {
       return <BlockListBlock {...props}></BlockListBlock>;
     }
     return (
-      <Card className="daxor-block-list__block-wrapper" inset>
+      <Card className="daxor-block-list__block-wrapper">
         <Inset clip="padding-box" side="all">
           <BlockListBlock {...props}></BlockListBlock>
         </Inset>
