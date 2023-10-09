0073e575c3fa95340380f9f9b3339e95e4c4be33
Heyde Moura <moura.heyde@gmail.com>
2023-10-08 23:36:17 -0300

    Add Container wrapper to editor and content view

--- src/components/BlockContentView.js
+++ src/components/BlockContentView.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { getBlockContent } from "@wordpress/blocks";
-import { Box, Flex, Card, ScrollArea } from "@radix-ui/themes";
+import { Box, Flex, Card, Container, ScrollArea } from "@radix-ui/themes";
 import "./BlockContentView.scss";
 
 const BlockContentView = ({ blocks }) => {
   return (
     <div className="daxor__block-content-view">
-      <Flex direction="column" gap="2" p="4">
-        {blocks.map((block, index) => {
-          const blockContent = getBlockContent(block);
+      <Container>
+        <Flex direction="column" gap="2">
+          {blocks.map((block, index) => {
+            const blockContent = getBlockContent(block);
 
-          return (
-            <div
-              className={`daxor__block-content-view__block daxor__block-content-view__block--${block.name}`}
-              key={`${index}-${block.name}`}
-              dangerouslySetInnerHTML={{ __html: blockContent }}
-            />
-          );
-        })}
-      </Flex>
+            return (
+              <div
+                className={`daxor__block-content-view__block daxor__block-content-view__block--${block.name}`}
+                key={`${index}-${block.name}`}
+                dangerouslySetInnerHTML={{ __html: blockContent }}
+              />
+            );
+          })}
+        </Flex>
+      </Container>
     </div>
   );
 };
