277f1ca6cf5e8b4b8d4f8b60c3a1dbe7d46981e4
Heyde Moura <moura.heyde@gmail.com>
2023-10-09 23:29:08 -0300

    Add block insertion with autocompletes

--- src/blocks/block-autocomplete/edit.jsx
+++ src/blocks/block-autocomplete/edit.jsx
@@ -1,15 +1,61 @@
-import { RichText, useBlockProps } from "@wordpress/block-editor";
+import { useSelect, useDispatch } from "@wordpress/data";
+import { useCallback, useRef, useEffect, useMemo } from "react";
+import {
+  BlockIcon,
+  RichText,
+  useBlockProps,
+  store as blockEditorStore,
+} from "@wordpress/block-editor";
+import { Flex, Text } from "@radix-ui/themes";
+import { store as blocksStore, createBlock } from "@wordpress/blocks";
+import "./edit-style.scss";
 
 const BlockAutcomplete = ({ attributes, setAttributes }) => {
+  const editableElRef = useRef(null);
+  const { getInserterItems } = useSelect(blockEditorStore);
+  const { insertBlock } = useDispatch(blockEditorStore);
+
+  const getOptionLabel = useCallback((blockType) => {
+    return blockType.title;
+  }, []);
+
+  const getOptionCompletion = useCallback(
+    (blockType) => {
+      const newBlock = createBlock(blockType.name);
+      insertBlock(newBlock);
+      setAttributes({ content: "" });
+    },
+    [insertBlock, setAttributes],
+  );
+
+  const completer = useMemo(
+    () => ({
+      name: "block-autocomplete",
+      triggerPrefix: "/",
+      options: getInserterItems(),
+      getOptionLabel: getOptionLabel,
+      getOptionCompletion: getOptionCompletion,
+    }),
+    [getInserterItems, getOptionLabel],
+  );
+
+  useEffect(() => {
+    // Focus on the editable element on mount
+    editableElRef.current.focus();
+    console.log(editableElRef.current);
+  }, []);
+
   return (
-    <div {...useBlockProps()}>
-      <RichText
-        tagName="p"
-        value={attributes.content}
-        onChange={(content) => setAttributes({ content })}
-        placeholder="Type / to choose a block"
-      />
-    </div>
+    <RichText
+      {...useBlockProps()}
+      ref={editableElRef}
+      tagName="p"
+      tabIndex="0"
+      value={attributes.content}
+      onChange={(content) => setAttributes({ content })}
+      placeholder="Type / to choose a block"
+      autocompleters={[completer]}
+    />
   );
 };
 
