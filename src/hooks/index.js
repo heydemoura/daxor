dd1803fe7cd54fa0b4c0c0b06ac8f99761609751
Heyde Moura <moura.heyde@gmail.com>
2023-10-09 23:29:26 -0300

    Exclude auto complete of being styles as a Card

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,28 +2,43 @@
  * Apply Block Editor hooks to customize the behavior of the editor.
  */
 import { addFilter } from "@wordpress/hooks";
-import DaxorBlockListBlock from "../components/DaxorBlockListBlock";
-// import DaxorInspectorControls from "../components/DaxorInspectorControls";
+import { createHigherOrderComponent } from "@wordpress/compose";
+import { Card, Inset } from "@radix-ui/themes";
 
 const withDefaultClassName = (className, blockName) => {
   const sanitizedClassName = blockName.replace("/", "-");
   return `${className} daxor-block__${sanitizedClassName} daxor-block`;
 };
 
+const withCustomBlockListBlock = createHigherOrderComponent(
+  (BlockListBlock) => (props) => {
+    const excludeBlocks = [
+      "core/column",
+      "core/columns",
+      "core/paragraph",
+      "daxor/radix-columns",
+      "daxor/block-autocomplete",
+    ];
+    if (excludeBlocks.indexOf(props.name) > -1) {
+      return <BlockListBlock {...props}></BlockListBlock>;
+    }
+    return (
+      <Card className="daxor-block-list__block-wrapper">
+        <Inset clip="padding-box" side="all">
+          <BlockListBlock {...props}></BlockListBlock>
+        </Inset>
+      </Card>
+    );
+  },
+  "withCustomBlockListBlock",
+);
+
 export default function registerEditorHooks() {
   addFilter(
     "editor.BlockListBlock",
     "daxor/block-list-block",
-    DaxorBlockListBlock,
+    withCustomBlockListBlock,
   );
-
-  // addFilter(
-  //   "editor.BlockEdit",
-  //   "daxor/inspector-controls",
-  //   DaxorInspectorControls,
-  // );
-  //
-
   addFilter(
     "blocks.getBlockDefaultClassName",
     "daxor/block-default-class-name",
